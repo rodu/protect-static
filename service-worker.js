3a90bf30c362ba1000cc1fffa0481495f6f5e0b9
rodu <3225111+rodu@users.noreply.github.com>
2021-10-21 11:21:40 +0100

    Creates initial proof of concept using service worker to decode clear data

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+async function decryptContent(response) {
+  const decrypted = decodeURIComponent(await response.text());
+
+  const { status, statusText, headers } = response;
+  return new Response(decrypted, {
+    status,
+    statusText,
+    headers,
+  });
+}
+
+self.addEventListener('fetch', (event) => {
+  const url = event.request.url;
+
+  if (url.match(/\/contents\/.+\.(j|cs)s$/)) {
+    event.respondWith(fetch(url).then(decryptContent));
+  }
+});
