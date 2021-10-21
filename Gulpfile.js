f4d737360aeae8e37eb74a4076b70d9641e91a66
rodu <3225111+rodu@users.noreply.github.com>
2021-10-21 16:36:28 +0100

    First implementation of encryption using sample password

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const gulpClean = require('gulp-clean');
 const tap = require('gulp-tap');
+const { Crypto } = require('node-webcrypto-ossl');
+
+const crypto = new Crypto();
 
 const srcFolder = 'app/**/*';
 const destFolder = 'dist';
@@ -23,17 +26,53 @@ function encrypt(content = '') {
  * files with the extensions matching the given list
  */
 function protect() {
-  return gulp
-    .src(srcFolder)
-    .pipe(
-      tap((file) => {
+  return new Promise((resolve, reject) => {
+    const stream = gulp.src(srcFolder).pipe(
+      tap(async (file) => {
         if (encryptExtensions.includes(file.extname)) {
-          file.contents = Buffer.from(encrypt(file.contents.toString()));
+          file.contents = Buffer.from(
+            await aesGcmEncrypt(file.contents.toString(), 'password')
+          );
+
+          resolve(stream);
+        } else {
+          resolve(stream);
         }
       })
-    )
-    .pipe(gulp.dest(destFolder));
+    );
+  }).then((stream) => stream.pipe(gulp.dest(destFolder)));
 }
 
 exports.protect = protect;
 exports.default = gulp.series(clean, protect);
+
+/**
+ * Encrypts plaintext using AES-GCM with supplied password, for decryption with aesGcmDecrypt().
+ *                                                                      (c) Chris Veness MIT Licence
+ *
+ * @param   {String} plaintext - Plaintext to be encrypted.
+ * @param   {String} password - Password to use to encrypt plaintext.
+ * @returns {String} Encrypted ciphertext.
+ *
+ * @example
+ *   const ciphertext = await aesGcmEncrypt('my secret text', 'pw');
+ *   aesGcmEncrypt('my secret text', 'pw').then(function(ciphertext) { console.log(ciphertext); });
+ */
+async function aesGcmEncrypt(plaintext, password) {
+  const pwUtf8 = new TextEncoder().encode(password); // encode password as UTF-8
+  const pwHash = await crypto.subtle.digest('SHA-256', pwUtf8); // hash the password
+
+  const iv = crypto.getRandomValues(new Uint8Array(12)); // get 96-bit random iv
+  const alg = { name: 'AES-GCM', iv: iv }; // specify algorithm to use
+  const key = await crypto.subtle.importKey('raw', pwHash, alg, false, [
+    'encrypt',
+  ]); // generate key from pw
+
+  const ptUint8 = new TextEncoder().encode(plaintext); // encode plaintext as UTF-8
+  const ctBuffer = await crypto.subtle.encrypt(alg, key, ptUint8); // encrypt plaintext using key
+
+  return (
+    Buffer.from(iv).toString('base64') +
+    Buffer.from(new Uint8Array(ctBuffer)).toString('base64')
+  );
+}
